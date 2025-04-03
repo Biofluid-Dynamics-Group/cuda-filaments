a5e27868bf6bdcdd82718dcfbc103b386ca66793
pzuritas <zurita.pablo@gmail.com>
2025-04-03 13:50:38 +0100

    running WGF with CUFCM to see if that helps, GMRES set to 1e-8

--- config.hpp
+++ config.hpp
@@ -144,12 +144,12 @@ extern std::string CUFCM_CONFIG_FILE_NAME;
   // If false, no such rotation ever occurs.
   // In the Platynaereis case this changes to an elastic variable.
 
-  #define WRITE_GENERALISED_FORCES false
+  #define WRITE_GENERALISED_FORCES true
   // If true, this simulation will save its generalised forces to file for use as the reference values.
   // It will also generate reference s-values for shape sequences which don't result in inextensible filaments.
   // NOTE: This will overwrite any existing reference files unless their names have been changed.
 
-  #define CILIA_IC_TYPE 1
+  #define CILIA_IC_TYPE 0
   // Valid options:
   // 0 = All cilia start in-phase with phase 0.
   // 1 = Cilia start with a (uniformly) random initial phase.
@@ -173,7 +173,7 @@ extern std::string CUFCM_CONFIG_FILE_NAME;
 
 #endif
 
-#define BODY_OR_SURFACE_TYPE 5
+#define BODY_OR_SURFACE_TYPE 2
 // Valid options:
 // 0 = An infinite plane wall at z = 0. This choice has some sub-types (see below). // 20240717:decrecated - only compatible with RPY
 // 1 = Deformed planes with 2 principal curvatures (partially implemented)
@@ -201,7 +201,7 @@ extern std::string CUFCM_CONFIG_FILE_NAME;
 
 #elif BODY_OR_SURFACE_TYPE==2 or BODY_OR_SURFACE_TYPE==4 or BODY_OR_SURFACE_TYPE==5
 
-  #define SEEDING_TYPE 3
+  #define SEEDING_TYPE 7
   // Valid options:
   // 0 = Filaments are evenly distributed over the surface.
   // 1 = Filaments are seeded in an equatorial band.
@@ -338,7 +338,7 @@ extern Real FREQ_SHIFT;  // Frequency percentage shift
 // 1: Use GMRES to solve the linear system at each iteration of Broyden's method.
 
 #define MAX_LINEAR_SYSTEM_ITER 500 // Maximum number of iterations used to solve the linear system in each mobility solve.
-#define LINEAR_SYSTEM_TOL 1e-7 // Relative tolerance in the linear system solves.
+#define LINEAR_SYSTEM_TOL 1e-8 // Relative tolerance in the linear system solves.
 
 #if SOLVER_TYPE==1
 
@@ -363,8 +363,8 @@ extern Real FREQ_SHIFT;  // Frequency percentage shift
 
 #else
 
-  #define STEPS_PER_PERIOD 500
-  #define SAVES_PER_PERIOD 500
+  #define STEPS_PER_PERIOD 25000
+  #define SAVES_PER_PERIOD 25000
 
 #endif
 
--- pyfile/driver/func.py
+++ pyfile/driver/func.py
@@ -14,9 +14,9 @@ class DRIVER:
         self.afix = ''
         self.inputfile = f""
 
-        self.category = f'gmres_test_e_7_cufcm/'
-        self.exe_name = f'cilia_1e-4'
-        self.date = '20250402'
+        self.category = f'wgf_sphere/'
+        self.exe_name = f'cilia_1e-8_wgf'
+        self.date = '20250403'
         self.dir = f"data/{self.category}{self.date}{self.afix}/"
 
         self.pars_list = {
@@ -94,13 +94,13 @@ class DRIVER:
                         tilt_angle = 0.0
 
                         nfil = num_fils
-                        nblob = 1024
+                        nblob = 9000
                         nseg = 20
                         ar = 8  # This is D/L, not R/L. This is for Platynaereis
                         # Since D/L = 8 and L = 20um, D = 160um
                         # In the simulation, L is around  49 units
                         period = 1
-                        spring_factor = 1e-3
+                        spring_factor = 1e-2
 
                         nx=500
                         ny=500
@@ -112,7 +112,7 @@ class DRIVER:
                         blob_x_dim=160*(i+1)
                         hex_num=2
                         reverse_fil_direction_ratio=0.0
-                        sim_length = 2.0
+                        sim_length = 1.0
                         f_eff = 0.3
                         theta_0 = 3.14159265359/2.1
                         freq_shift = 0.0  # This was for a frequency gradient study
@@ -224,8 +224,8 @@ class DRIVER:
 
             command = f"export OPENBLAS_NUM_THREADS=1; \
                         export CUDA_VISIBLE_DEVICES={self.cuda_device}; \
-                        ./bin/{self.exe_name}"
-                        # nohup ./bin/{self.exe_name} > {precon}_nohup_k{k}.out &"
+                        nohup ./bin/{self.exe_name} > nohup.out &"
+                        # ./bin/{self.exe_name}"
             
             # on ic hpc
             # command = f"export OPENBLAS_NUM_THREADS=1; \
