dcb05b091cc92e9f3a0a97aa3f0f7728a3a87d3f
pzuritas <zurita.pablo@gmail.com>
2025-07-16 15:07:12 +0100

    random conditions done

--- pyfile/driver/func.py
+++ pyfile/driver/func.py
@@ -14,7 +14,7 @@ class DRIVER:
         self.afix = ''
         self.inputfile = f""
 
-        self.category = f'prescribed_random_5/'
+        self.category = f'prescribed_random_10/'
         self.exe_name = f'cilia_1e-7'
         self.date = '20250715'
         self.dir = f"data/{self.category}{self.date}{self.afix}/"
