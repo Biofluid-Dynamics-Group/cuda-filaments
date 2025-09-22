71a03522b32a4877b4aec0fc1066a509d3ed2951
pzuritas <zurita.pablo@gmail.com>
2025-09-22 15:57:04 +0100

    corrected the gap angle

--- config.hpp
+++ config.hpp
@@ -159,10 +159,10 @@ extern std::string CUFCM_CONFIG_FILE_NAME;
   // 3 = Ishikawa MCW
   // 5 = Read from a file (default - we can define arbitrary initial conditions in python)
 
-  #define PLATY_GROUPS true // Divide a sphere into 12 segments of frequency w/ gaps
+  #define PLATY_GROUPS false // Divide a sphere into 12 segments of frequency w/ gaps
   #if PLATY_GROUPS
     #define NUM_PLATY_GROUPS 12             // number of groups
-    #define PLATY_GROUP_GAP_ANG 0.106       // angular gap between groups (rad)
+    #define PLATY_GROUP_GAP_ANG 0.03375     // angular gap between groups (rad)
   #endif
 
 
